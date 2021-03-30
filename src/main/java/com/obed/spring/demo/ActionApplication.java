ef82258bc4a0aab627f5a12480ff9f7ccb7631dd
obed <ouyanob@oocl.com>
2021-03-30 14:57:31 +0800

    Initial commit

---
+++ src/main/java/com/obed/spring/demo/ActionApplication.java
@@ -0,0 +1,16 @@
+package com.obed.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author OUYANOB
+ */
+@SpringBootApplication
+public class ActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/obed/spring/demo/ActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obed.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
